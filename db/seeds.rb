d4d91c08f9373844c4e38f66c918f900d01c45f3
krk324 <krk32488@gmail.com>
2014-07-28 16:15:45 -0400

    Profile page now displays user's basic info. and uploaded photos.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 alex = User.create(email: 'alex@example.com', password: 'password')
 
+Image.create({
+  :user_id => 1,
+  :sharedimg => File.new("#{Rails.root}/app/assets/images/blackcats-lg.jpg"),
+})
