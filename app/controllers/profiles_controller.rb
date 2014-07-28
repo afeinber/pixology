aaf8e06926b0f93cf0fe439f5902b262b02cf2dd
krk324 <krk32488@gmail.com>
2014-07-28 19:07:52 -0400

    Set default avatar.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,4 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
 
   def show
     @profile = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@ class User < ActiveRecord::Base
   has_many :notifications, dependent: :destroy
   has_many :votes, dependent: :destroy
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('books-thumb.jpg')
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
