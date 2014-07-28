97f6daf2abcfb2ab4d905b3bb099d2777b300c05
krk324 <krk32488@gmail.com>
2014-07-28 16:57:36 -0400

    My profile page now display followings and followers. Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,29 @@
-alex = User.create(email: 'alex@example.com', password: 'password')
+alex = User.create(username:'Alex', email: 'alex@example.com', password: 'password')
+kaga = User.create(username: 'Kaga', email: 'kaga@example.com', password: 'password')
+katryn = User.create(username: 'Katryn', email: 'katryn@example.com', password: 'password')
 
-Image.create({
-  :user_id => 1,
-  :sharedimg => File.new("#{Rails.root}/app/assets/images/blackcats-lg.jpg"),
-})
+
+Image.create(
+  user_id: 1,
+  sharedimg: File.new("#{Rails.root}/app/assets/images/blackcats-lg.jpg")
+)
+
+#Alex following Katryn and Kaga
+Follow.create(
+  user_id: 1,
+  followee_id: 2
+)
+
+Follow.create(
+  user_id: 1,
+  followee_id: 3
+)
+
+#Alex followed by Kaga
+
+Follow.create(
+  user_id: 2,
+  followee_id: 1
+)
+
+
