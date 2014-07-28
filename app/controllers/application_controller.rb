37fd5132d79fd6613192022c2a57cfe638c291c5
krk324 <krk32488@gmail.com>
2014-07-28 14:59:44 -0400

    App now saves user profile edits to the database.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :avatar) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :avatar, :blurb) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :avatar, :blurb,:current_password) }
   end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
