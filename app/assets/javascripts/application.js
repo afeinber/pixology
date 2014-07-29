f8a2756d7c38e9955f4b35770cbac4e7db2719f5
testkitschen <katryn.mcintosh@gmail.com>
2014-07-28 20:22:04 -0400

    Add "add tag" to a separate div section so it doesn't jump to the top of the form. Add slow sexy fade in for a new tag field.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,5 +39,14 @@ $(document).ready(function() {
       $(this).children('i').addClass('fa-angle-double-down');
     }
   });
+  $('#category')
+      .on('cocoon:before-insert', function(e,category_to_be_added) {
+        category_to_be_added.fadeIn('slow');
+      })
+      .on('cocoon:before-remove', function(e, category) {
+        // allow some time for the animation to complete
+        $(this).data('remove-timeout', 1000);
+        category.fadeOut('slow');
+      });
 });
 
