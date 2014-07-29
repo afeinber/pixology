6bd104d57f9b901167a1bd6063d907c6beab28e2
krk324 <krk32488@gmail.com>
2014-07-28 21:03:06 -0400

    Add destory methods to images controller.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,18 +1,28 @@
 class ImagesController < ApplicationController
-   def show
+
+  def show
     @image = Image.find(params[:id])
   end
+
   def new
     @image = Image.new
   end
+
   def create
     @image = Image.new(image_params)
 
     if @image.save
       redirect_to @image, notice: 'Image was successfully added.'
-     else
-       render action: 'new'
+    else
+      render action: 'new'
     end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+
+    head :no_content
   end
 
   private
