1425db7b5d6fc4cf7644a4a7758095dcb9eda539
krk324 <krk32488@gmail.com>
2014-07-31 11:11:48 -0400

    Fix problem where different image types of avatar don't show up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_many :notifications, dependent: :destroy
   has_many :votes, dependent: :destroy
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('books-thumb.jpg')
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/assets/book-:style.jpg'
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def self.member_length(user_create_time)
