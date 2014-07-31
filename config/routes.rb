96ab2f534ee80febe7970fabf6add10b8a53d283
afeinber <alexandernfeinberg@gmail.com>
2014-07-31 15:59:31 -0400

    fix routes to normal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'images#new'
+  root 'home#index'
 
   resources :categories, only: [:create,:destroy]
   resources :favorites, only: [:create,:destroy]
