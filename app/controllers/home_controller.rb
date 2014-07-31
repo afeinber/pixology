d97aa6e5994ec7d9b0b13ecf1cbd444c6b8814fa
testkitschen <katryn.mcintosh@gmail.com>
2014-07-31 14:58:45 -0400

    fix the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @hot_right_now = Image.hot_right_now
-    @categories = defined?(current_user) ? current_user.categories : []
+    @categories = current_user.present? ? current_user.categories : []
     @fresh_pics = Image.rand_images
   end
 end
